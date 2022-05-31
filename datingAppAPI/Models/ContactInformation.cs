ca0c17619464ab8e43247e0fe8944c7920a1f94f
Afsanehbajoori <afsan.bajoori@hotmail.com>
2022-05-31 13:33:09 +0200

    add objects

--- datingAppAPI/Models/ContactInformation.cs
+++ datingAppAPI/Models/ContactInformation.cs
@@ -1,6 +1,24 @@
-﻿namespace datingAppAPI.Models
+﻿using System;
+
+namespace datingAppAPI.Models
 {
     public class ContactInformation
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Town { get; set; }
+        public string Country { get; set; }
+        public int PostNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+
+
     }
 }
--- datingAppAPI/Models/Login.cs
+++ datingAppAPI/Models/Login.cs
@@ -1,6 +1,16 @@
-﻿namespace datingAppAPI.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace datingAppAPI.Models
 {
     public class Login
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [Timestamp]
+        public DateTime LoginTime { get; set; }
+
     }
 }
